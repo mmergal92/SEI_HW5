2d0c7ec47269b0d69228b8c0a6095e383edaf6c3
mmergal92 <mmergal92@gmail.com>
2021-03-21 00:03:39 -0400

    I added my first element and it was fun

--- app.js
+++ app.js
@@ -5,5 +5,9 @@
 // };
 
 $(() => {
-    const $div = $('<div>').prop('id','container')
+   const $container = $('#container');
+   console.log($container);
+   const $h1 = $('<h1>').text('Hogwarts');
+   $container.append($h1);
+   console.log($h1);
 });
