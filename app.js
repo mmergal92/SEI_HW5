7358d67f6aaffba991c82268e48f5e5e38fbf2ba
mmergal92 <mmergal92@gmail.com>
2021-03-21 00:21:21 -0400

    Commit 3 - I added list elements. That code didn't seem very dry.

--- app.js
+++ app.js
@@ -16,4 +16,18 @@ $(() => {
    $container.append($h3);
    const $h4 = $('<h4>').prop("class","dog").text('Bruno');
    $container.append($h4);
+   const $ul = $('<ul>').attr('storage', 'trunk');
+   $container.append($ul)
+   const $li1 = $('<li>').text('beer better')
+   $ul.append($li1)
+   const $li2 = $('<li>').prop("class","secret").text('invisibility cloak')
+   $ul.append($li2)
+   const $li3 = $('<li>').prop("class","secret").text('magic map')
+   $ul.append($li3)
+   const $li4 = $('<li>').prop("class","secret").text('time turner')
+   $ul.append($li4)
+   const $li5 = $('<li>').prop("class","dog").text('leash')
+   $ul.append($li5)
+   const $li6 = $('<li>').text('Bertie Bott/s Every Flavor [Jelly] Beans')
+   $ul.append($li6)
 });
