53df95800031cec81d1b43c2150314262b0e8c5a
mmergal92 <mmergal92@gmail.com>
2021-03-21 00:09:24 -0400

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- app.js
+++ app.js
@@ -10,4 +10,10 @@ $(() => {
    const $h1 = $('<h1>').text('Hogwarts');
    $container.append($h1);
    console.log($h1);
+   const $h2 = $('<h2>').text('Maria');
+   $container.append($h2);
+   const $h3 = $('<h3>').text('Gryffindor');
+   $container.append($h3);
+   const $h4 = $('<h4>').prop("class","dog").text('Bruno');
+   $container.append($h4);
 });
